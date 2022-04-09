195ceadd63845bea67c228d9eeba8efa13532876
Lucas-Feat <lucas.feat.lf@gmail.com>
2022-04-09 11:52:48 +0200

    footer ok

---
+++ c
@@ -0,0 +1,23 @@
+[33mcommit b566abc0f1dce6b347977c443c302b6321e8cbbb[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Lucas FEAT <lucas.feat2021@campus-eni.fr>
+Date:   Fri Apr 8 16:04:52 2022 +0200
+
+    footer presque ok
+
+[33mcommit 1794c45689277285d69cd3d07bd4e4811b485c22[m
+Author: Lucas FEAT <lucas.feat2021@campus-eni.fr>
+Date:   Fri Apr 8 13:59:45 2022 +0200
+
+    scrollToTop ok
+
+[33mcommit 44276bd7800d21af82f859464cb17d2b86d5dbe7[m
+Author: Lucas FEAT <lucas.feat2021@campus-eni.fr>
+Date:   Fri Apr 8 13:38:51 2022 +0200
+
+    first commit
+
+[33mcommit 33de918e8d2974a42df522f70e38a984dd79f198[m
+Author: Lucas FEAT <lucas.feat2021@campus-eni.fr>
+Date:   Fri Apr 8 09:27:40 2022 +0200
+
+    initial commit
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
-import { PreviewCarouselComponent } from './preview-carousel/preview-carousel.component';
+import { NavComponent } from './header/nav/nav.component';
+import { PreviewCarouselComponent } from './header/preview-carousel/preview-carousel.component';
 import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, PreviewCarouselComponent, ScrollToTopComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    PreviewCarouselComponent,
+    ScrollToTopComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
