57e8f1099e5d2e66e1415c92620574ccb152f3d8
Lucas-Feat <lucas.feat.lf@gmail.com>
2022-04-09 14:13:29 +0200

    div a-propos ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 import { MainComponent } from './main/main.component';
 import { ContactComponent } from './main/contact/contact.component';
 import { ServicesComponent } from './main/services/services.component';
+import { AProposComponent } from './main/a-propos/a-propos.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ServicesComponent } from './main/services/services.component';
     MainComponent,
     ContactComponent,
     ServicesComponent,
+    AProposComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
