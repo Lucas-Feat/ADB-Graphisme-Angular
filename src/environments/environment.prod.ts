0490ceb40ef4d637f94a9bd3e9295b82f110651e
Lucas-Feat <lucas.feat.lf@gmail.com>
2022-04-10 17:32:14 +0200

    test deploiement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBHx9GzA3bMaSAFhipuds8NTerTTtOYb2k',
+    authDomain: 'adb-graphisme-bfef3.firebaseapp.com',
+    databaseURL:
+      'https://adb-graphisme-bfef3-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'adb-graphisme-bfef3',
+    storageBucket: 'adb-graphisme-bfef3.appspot.com',
+    messagingSenderId: '83502773285',
+    appId: '1:83502773285:web:e9a90297c0cfe8d215fbcc',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBHx9GzA3bMaSAFhipuds8NTerTTtOYb2k',
+    authDomain: 'adb-graphisme-bfef3.firebaseapp.com',
+    databaseURL:
+      'https://adb-graphisme-bfef3-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'adb-graphisme-bfef3',
+    storageBucket: 'adb-graphisme-bfef3.appspot.com',
+    messagingSenderId: '83502773285',
+    appId: '1:83502773285:web:e9a90297c0cfe8d215fbcc',
+  },
 };
 
 /*
